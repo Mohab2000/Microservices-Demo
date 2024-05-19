1dba0816f9ab3f59c1d1aa76b13cd6549bba149a
Mohab Ahmed Mohamed | Ejada Systems Ltd. <mohabahmed20005@gmail.com>
2024-05-19 13:58:30 +0300

    Fetching and Listing comments & posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import PostCreate from "./PostCreate";
+import PostList from "./PostList";
 export default function App() {
   return (
     <div className="container">
       <h1>Create Post</h1>
       <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
   );
 }
---
+++ client/src/CommentList.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+export default function CommentList({ postId }) {
+  const [comments, setComments] = useState([]);
+  const fetchData = async () => {
+    const res = await axios.get(
+      `http://localhost:4001/posts/${postId}/comments`
+    );
+    setComments(res.data);
+  };
+  useEffect(() => {
+    fetchData();
+  });
+
+  const renderedComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+}
