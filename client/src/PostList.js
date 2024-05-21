1df021b1279b0a864588b40d976ab51eb8d6c7b2
Mohab Ahmed Mohamed | Ejada Systems Ltd. <mohabahmed20005@gmail.com>
2024-05-21 13:06:54 +0300

    Used query service in client

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -6,7 +6,7 @@ import CommentList from "./CommentList";
 export default function PostList() {
   const [posts, setPosts] = useState({});
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:4000/posts");
+    const res = await axios.get("http://localhost:4002/posts");
     setPosts(res.data);
   };
   useEffect(() => {
@@ -22,7 +22,7 @@ export default function PostList() {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
+          <CommentList comments={post.comments} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
