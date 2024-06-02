a1b144f1b0a776dcca6aaa0c369471612eaf9ac3
Mohab2000 <mohabahmed20005@gmail.com>
2024-06-02 22:00:43 +0300

    Configruations for deployment done

--- client/src/CommentCreate.js
+++ client/src/CommentCreate.js
@@ -5,7 +5,7 @@ export default function CommentCreate({ postId }) {
   const [content, setContent] = useState("");
   const onSubmit = async (event) => {
     event.preventDefault();
-    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+    await axios.post(`http://posts.com/posts/${postId}/comments`, {
       content,
     });
     setContent("");
--- client/src/PostList.js
+++ client/src/PostList.js
@@ -6,7 +6,7 @@ import CommentList from "./CommentList";
 export default function PostList() {
   const [posts, setPosts] = useState({});
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:4002/posts");
+    const res = await axios.get("http://posts.com/posts");
     setPosts(res.data);
   };
   useEffect(() => {
